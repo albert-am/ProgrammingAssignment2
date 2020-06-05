29d504fac8394f6e25d384e8ad489c97e129d688
albert-am <me19mtech11028@iith.ac.in>
2020-06-05 18:14:32 +0530

    Edited the template and addeed the code for inversion of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##An invertible matrix is generated and passed.Careful to pass invertible matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y)
+  {
+    x<<-y
+    m<<-NULL
+  }
+  get<-function()
+  {
+    x
+  }
+  setinverse<-function(k)
+  {
+    m<<-k
+  }
+  getinverse<-function()
+  {
+    m
+  }
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## To draw from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getinverse()
+  if(!is.null(m))
+  {
+    message("getting cached inverse")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data)
+  m
 }
